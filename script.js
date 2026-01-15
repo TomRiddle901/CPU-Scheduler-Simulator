cb938e9ce6274a96ac0e758a48b50b24edfca07f
Tommaso <tommaso.mangialardo@hotmail.com>
2026-01-15 12:33:35 +0100

    Generazione della tabella

--- script.js
+++ script.js
@@ -8,8 +8,29 @@ form.addEventListener('submit', function(event){
     event.preventDefault();
 
     // Leggi i valori del form
-    const pid = document.getElementById("pidMain");
-    const arrival = document.getElementById("arrivalMain");
-    const burst = document.getElementById("burstMain");
-    
+    const pid = document.getElementById("pidMain").value;
+    const arrival = parseInt(document.getElementById("arrivalMain").value);
+    const burst = parseInt(document.getElementById("burstMain").value);
+    const priority = parseInt(document.getElementById("priorityMain").value);
+
+    // Inserisci processo nell'array
+    processes.push({id: pid, arrival: arrival, burst: burst, priority: priority});
+
+    // Mostra la sezione output
+    outputSection.hidden = false;
+
+    // Aggiorna la tabella
+    tableBody.innerHTML = '';
+    processes.forEach(proc => {
+        const row = `<tr>
+        <td>${proc.id}<td>
+        <td>${proc.arrival}<td>
+        <td>${proc.burst}<td>
+        <td>${proc.priority}<td>
+        <tr>`;
+        tableBody.innerHTML += row;
+    });
+
+    // Resetta form
+    form.reset();
 });
